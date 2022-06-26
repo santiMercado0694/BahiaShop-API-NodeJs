7f198c9c3124b51a202b9a894a184ae58738271b
santiMercado0694 <santimercado06_94@hotmail.com>
2022-06-26 16:30:18 -0300

    cambios menores

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -6,7 +6,7 @@ const getContentCart = async(req, res) => {
     if(response.rows.length > 0){
         res.status(200).json(response.rows);
     }else{
-        res.status(404).json({error: 'No se encontraron productos'});
+        res.status(404).json({error: 'No se encontraron productos en el carrito'});
     }
 };
 
