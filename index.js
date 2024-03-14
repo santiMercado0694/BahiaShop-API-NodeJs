277b9c49cb1874b7e5563f82c7cb8278c66dbfb8
santiMercado0694 <santimercado06_94@hotmail.com>
2024-03-14 17:56:10 -0300

    manejo de web push con la API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: "src/.env" })
 const swagger = require('./swagger.js');
 const cors = require('cors');
 const compression = require('compression');
+const webpush = require('webpush');
 
 const express = require('express'),
       bodyParser = require('body-parser'),
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(compression());
 
+webpush.setVapidDetails(VAPID_SUBJECT,VAPID_PUBLIC_KEY,VAPID_PRIVATE_KEY);
+
 //Swagger
 app.use(
     '/swagger',
@@ -19,8 +22,24 @@ app.use(
     swagger.setup
   );
 
-//Routes
-app.use(require('./src/routes.js'))
+//Routes Ecommerce
+app.use(require('./src/routes.js'));
+
+//Routes Web Push
+app.post('/subscribe', (req, res) => {
+  // Get pushSubcription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload
+  const payload = JSON.stringify({title: 'Push Test'});
+
+  // Pass object into sendNotification
+  webpush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
+
 
 const PORT = process.env.PORT || 3306;
 
