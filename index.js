865e63879cef2113040d5747fa5ca924a522175b
santiMercado0694 <santimercado06_94@hotmail.com>
2024-05-22 17:22:34 -0300

    limpieza de archivos que no vamos a necesitar

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: "./.env" });
-const swagger = require('./swagger.js');
 const cors = require('cors');
 const compression = require('compression');
-const webpush = require('web-push');
 
 const express = require('express'),
       bodyParser = require('body-parser'),
@@ -13,37 +11,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(compression());
 
-const VAPID_SUBJECT = process.env.VAPID_SUBJECT;
-const VAPID_PUBLIC_KEY = process.env.VAPID_PUBLIC_KEY;
-const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY;
-
-webpush.setVapidDetails(VAPID_SUBJECT,VAPID_PUBLIC_KEY,VAPID_PRIVATE_KEY);
-
-//Swagger
-app.use(
-    '/swagger',
-    swagger.serve, 
-    swagger.setup
-  );
-
 //Routes Ecommerce
 app.use(require('./src/routes.js'));
-
-//Routes Web Push
-app.post('/subscribe', (req, res) => {
-  // Get pushSubcription object
-  const subscription = req.body;
-
-  // Send 201 - resource created
-  res.status(201).json({});
-
-  // Create payload
-  const payload = JSON.stringify({title: 'Push Test'});
-
-  // Pass object into sendNotification
-  webpush.sendNotification(subscription, payload).catch(err => console.error(err));
-});
-
 
 const PORT = process.env.PORT || 3306;
 
